84d850533b8d0db7f0bcb6bad620d953895cf437
FoxLib <hplus.foxtail@gmail.com>
2020-07-28 14:18:36 +0300

    LANTONI!

--- 01bootstart/main.asm
+++ 01bootstart/main.asm
@@ -2,6 +2,9 @@
         org     0x8000
         include "inc/macro.asm"
 
+	cli
+	cld
+
         ; Инициализация
         xor     ax, ax
         xor     sp, sp
@@ -9,16 +12,4 @@
         mov     ss, ax
         mov     ax, $b800
         mov     es, ax
-
-        invoke  cls,$0720
-        invoke  locate,2,49
-        invoke  printf,h
-
-@@:     call    getch
-        call    print
-        jmp     @b
-
-h:      db "Core decompressing initializing procedure...", 0
-
-        include "inc/stdio.asm"
-        include "inc/keyboard.asm"
+	jmp	$
