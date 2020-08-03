0ca8ac88e4f112a5cc591220234399aa1d53288b
FoxLib <hplus.foxtail@gmail.com>
2020-08-03 09:06:12 +0300

    Просто добавил XP-интерфейс небольшой

--- 02nifedos/src/gui.c
+++ 02nifedos/src/gui.c
@@ -9,12 +9,17 @@ void gui_init() {
 void gui_draw_window(char* name, int x, int y, int w, int h) {
 
     int x2 = x + w, y2 = y + h;
-    block(x, y, x2, y2, C(0xd5d5d5));
     lineb(x, y, x2, y2, 0);
-    block(x+2, y+2, x2-2, y+19, C(0x0084b5));
+
+    // --
+    for (int i = 0; i < 26; i++) {
+        block(x, y+i, x2, y+i, C(GUITopGradient[i]));
+    }
+
+    block(x, y+26, x2, y2, C(0xd5d5d5));
 
     bold(1);
     font(FONT_TREBUCHETMS_14);
-    locate(x + 7, y + 5); colorfr(CL_BLACK); print(name);
-    locate(x + 6, y + 4); colorfr(CL_WHITE); print(name);
+    locate(x + 7, y + 8); colorfr(CL_BLACK); print(name);
+    locate(x + 6, y + 7); colorfr(CL_WHITE); print(name);
 }
